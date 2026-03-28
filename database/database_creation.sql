4efb6113359a1f71b9cd887f77c51c6c2aee3466
chhoflac <chhoflac@student.42.fr>
2026-03-28 16:09:14 +0100

    adding friendship realtion table

--- database/database_creation.sql
+++ database/database_creation.sql
@@ -1,5 +1,5 @@
 CREATE TABLE account (
-  account_id INTEGER PRIMARY KEY,
+  account_id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50),
   last_name VARCHAR(50),
   email VARCHAR(255) UNIQUE NOT NULL,
@@ -15,12 +15,13 @@ CREATE TABLE account (
 );
 
 CREATE TABLE game (
-  game_id INTEGER PRIMARY KEY,
+  game_id INTEGER PRIMARY KEY AUTO_INCREMENT,
   players_count INTEGER NOT NULL
+  played_at TIMESTAMP
 );
 
 CREATE TABLE game_player (
-  game_player_id INTEGER PRIMARY KEY,
+  game_player_id INTEGER PRIMARY KEY AUTO_INCREMENT,
   game_id INTEGER NOT NULL,
   account_id INTEGER NOT NULL,
   score INTEGER NOT NULL,
@@ -29,3 +30,13 @@ CREATE TABLE game_player (
   FOREIGN KEY (account_id) REFERENCES account(account_id),
   UNIQUE (game_id, account_id)
 );
+
+CREATE TABLE friendship (
+	account_id_1 INTEGER NOT NULL,
+	account_id_2 INTEGER NOT NULL,
+	FOREIGN KEY (account_id_1) REFERENCES account(account_id),
+    FOREIGN KEY (account_id_2) REFERENCES account(account_id),
+	PRIMARY KEY(account_id_1, account_id_2),
+	CHECK (account_id_1 < account_id_2)
+);
+
