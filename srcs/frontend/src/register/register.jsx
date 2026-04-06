e552212005c33a4f45dfc3a40d4c1b7e269e6a30
chhoflac <chhoflac@student.42.fr>
2026-04-06 09:14:02 +0200

    adding labels instead of span

--- srcs/frontend/src/register/register.jsx
+++ srcs/frontend/src/register/register.jsx
@@ -19,20 +19,20 @@ function Register(){
 	return (
 		<div className="d-flex justify-content-center align-items-center min-vh-100">
 			<form className="register-form d-flex flex-column gap-3" onSubmit={manageSubmit} >
-				<span className="register-text">Name : </span>
-				<input type="text" name="name" value={name} placeholder="Name" className="form-control register-input"   onChange={(e) => setName(e.target.value)}/>
-				<span className="register-text">Last name : </span>
-				<input type="text" name="last_name" value={last_name} placeholder="Last name" className="form-control register-input"   onChange={(e) => setLastName(e.target.value)}/>
-				<span className="register-text">Pseudo :</span>
-				<input type="text" name="pseudo" value={pseudo} placeholder="Pseudo" className="form-control register-input"   onChange={(e) => setPseudo(e.target.value)}/>
-				<span className="register-text">Email :</span>
-				<input type="email" name="email" value={email} placeholder="example@example.com" className="form-control register-input" onChange={(e) => setEmail(e.target.value)}/>
-				<span className="register-text">Password :</span>
-				<input type="password" name="password" value={password} placeholder="Password" className="form-control register-input" onChange={(e) => setPassword(e.target.value)}/>
-				<span className="register-text">Re-enter password :</span>
-				<input type="password" name="password_verify" value={passwordVerify} placeholder="password verify" className="form-control register-input" onChange={(e) => setPasswordVerify(e.target.value)}/>
-				<span className="register-text">Birthdate :</span>
-				<input type="date" name="birthdate" value={birthdate} className="form-control register-input" onChange={(e) => setBirthDate(e.target.value)}/>
+				<label htmlFor="name" className="register-text">Name:</label>
+				<input type="text" name="name" value={name} placeholder="Name" className="form-control register-input" id="name" onChange={(e) => setName(e.target.value)}/>
+				<label htmlFor="last_name" className="register-text">Last name:</label>
+				<input type="text" name="last_name" value={last_name} placeholder="Last name" className="form-control register-input" id="last_name" onChange={(e) => setLastName(e.target.value)}/>
+				<label htmlFor="pseudo" className="register-text">Pseudo:</label>
+				<input type="text" name="pseudo" value={pseudo} placeholder="Pseudo" className="form-control register-input" id="pseudo" onChange={(e) => setPseudo(e.target.value)}/>
+				<label htmlFor="email" className="register-text">Email:</label>
+				<input type="email" name="email" value={email} placeholder="example@example.com" className="form-control register-input" id="email" onChange={(e) => setEmail(e.target.value)}/>
+				<label htmlFor="password" className="register-text">Password:</label>
+				<input type="password" name="password" value={password} placeholder="Password" className="form-control register-input" id="password" onChange={(e) => setPassword(e.target.value)}/>
+				<label htmlFor="password_verify" className="register-text">Password verify: </label>
+				<input type="password" name="password_verify" value={passwordVerify} placeholder="password verify" className="form-control register-input" id="password_verify" onChange={(e) => setPasswordVerify(e.target.value)}/>
+				<label htmlFor="birthdate" className="register-text">Birthdate:</label>
+				<input type="date" name="birthdate" value={birthdate} className="form-control register-input" id="birthdate" onChange={(e) => setBirthDate(e.target.value)}/>
 				<button type="submit" className="btn btn-primary register-button">Register</button>
 			</form>
 		</div>
