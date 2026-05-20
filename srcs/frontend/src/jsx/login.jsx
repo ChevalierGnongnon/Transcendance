c1662755e9cb336ac510a20eeb873943aa8a6ad2
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-20 19:58:36 +0200

    translation starting using i18n.js

--- srcs/frontend/src/jsx/login.jsx
+++ srcs/frontend/src/jsx/login.jsx
@@ -18,12 +18,12 @@ function Login() {
 	return (
 		<div className="d-flex justify-content-center align-items-center min-vh-100">
 			<form className="login-form d-flex flex-column gap-3" onSubmit={manageSubmit} >
-				<h1 className="login-title">t('login.title')</h1>
-				<label htmlFor="login" className="form-text">t('login.label_login')</label>
-				<input type="text" name="name" value={login} placeholder="login or email" className="form-control form-input" id="login" onChange={(e) => setLogin(e.target.value)} />
-				<label htmlFor="password" className="form-text">t('common.label_password')</label>
-				<input type="password" name="last_name" value={password} placeholder="Password" className="form-control form-input" id="password" onChange={(e) => setPassword(e.target.value)} />
-				<button type="submit" className="btn btn-primary form-button">Login</button>
+				<h1 className="login-title">{t('login.title')}</h1>
+				<label htmlFor="login" className="form-text">{t('login.label_login')}</label>
+				<input type="text" name="name" value={login} placeholder={t('login.placeholder_login')} className="form-control form-input" id="login" onChange={(e) => setLogin(e.target.value)} />
+				<label htmlFor="password" className="form-text">{t('common.label_password')}</label>
+				<input type="password" name="last_name" value={password} placeholder={t('common.placeholder_password')} className="form-control form-input" id="password" onChange={(e) => setPassword(e.target.value)} />
+				<button type="submit" className="btn btn-primary form-button">{t('login.button')}</button>
 			</form>
 		</div>
 	);
