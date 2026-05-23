1aadda49fb0f468cd097c7b61fa842295ed844cc
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-23 16:43:22 +0200

    routes in main.jsx

--- srcs/frontend/src/jsx/main.jsx
+++ srcs/frontend/src/jsx/main.jsx
@@ -5,11 +5,16 @@ import Login from "./login";
 import i18n from "../../localisation/i18n.js"
 import Header from "./header";
 import Register from "./register.jsx";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Header />
-		<Login />
+		<BrowserRouter>
+			<Header/>
+			<Routes>
+				<Route path="/login" element={<Login/>}/>
+				<Route path="/register" element={<Register/>}/>
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
