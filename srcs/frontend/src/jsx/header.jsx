9bacc8b51f0f8e60236a094b26f1aa99d555a6ee
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-23 17:11:11 +0200

    rooting for register and login done

--- srcs/frontend/src/jsx/header.jsx
+++ srcs/frontend/src/jsx/header.jsx
@@ -1,7 +1,10 @@
 import "../scss/common-classes.scss"
 import i18n from '../../localisation/i18n.js'
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 function Header(){
+    const {t} = useTranslation();
     return (
         <header className="common-head d-flex gap-2 p-2">
             <input type="button" value="Français" className="btn btn-secondary btn-sm" onClick={() => { i18n.changeLanguage('fr'); localStorage.setItem('lang', 'fr'); }}/>
@@ -9,6 +12,10 @@ function Header(){
             <input type="button" value="Deutsch" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('de'); localStorage.setItem('lang', 'de'); }}/>
             <input type="button" value="Русский" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('ru'); localStorage.setItem('lang', 'ru'); }}/>
             <input type="button" value="українська" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('uk'); localStorage.setItem('lang', 'uk'); }}/>
+            <div className="ms-auto d-flex gap-2">
+                <Link to="/login" className="btn btn-secondary btn-sm">{t('login.button')}</Link>
+                <Link to="/register" className="btn btn-secondary btn-sm">{t('register.button')}</Link>
+            </div>
         </header>
     );
 }
--- srcs/frontend/src/jsx/main.jsx
+++ srcs/frontend/src/jsx/main.jsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 			<Routes>
 				<Route path="/login" element={<Login/>}/>
 				<Route path="/register" element={<Register/>}/>
+				<Route path="/" element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
