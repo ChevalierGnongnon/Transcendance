40eb7f205a1d5201f76f5007de291211b921261c
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-26 14:07:48 +0200

    mariadb and express connexion ok

--- srcs/backend/src/db-connexion.js
+++ srcs/backend/src/db-connexion.js
@@ -1,1 +1,10 @@
 const mysql = require('mysql2');
+const fs = require('fs');
+const password = fs.readFileSync('/run/secrets/mysql_password', 'utf8').trim();
+
+const pool = mysql.createPool({host: 'mariadb', 
+    database: process.env.MYSQL_DATABASE, 
+    user: process.env.MYSQL_USER,
+    password: password});
+
+module.exports=pool;
