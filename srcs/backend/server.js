57d88695442da9c181f583d41f7db3db3defd737
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-27 10:20:27 +0200

    adding database connexion

--- srcs/backend/server.js
+++ srcs/backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const database = require('./src/db-connexion.js');
 const app = express();
 const port = 3000;
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK')
-})
+});
 
 app.listen(port, () => {
     console.log("express ready");
-})
+});
+
+database.query('SELECT 1', (err, results) => {
+    if (err) {
+        console.error('DB connection failed:', err);
+    } else {
+        console.log('DB connected!');
+    }
+});
