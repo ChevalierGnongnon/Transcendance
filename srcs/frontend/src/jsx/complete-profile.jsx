13ae1b97b8965965369da2660dcf0abb90a98419
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-27 14:53:09 +0200

    localisation for file upload

--- srcs/frontend/src/jsx/complete-profile.jsx
+++ srcs/frontend/src/jsx/complete-profile.jsx
@@ -18,6 +18,8 @@ function CompleteYourProfile(){
 				<h1 className="login-title">{t('complete-your-profile.title')}</h1>
 				<label htmlFor="pseudo" className="form-text">{t('complete-your-profile.label-pseudo')}</label>
 				<input type="text" name="name" value={pseudo} placeholder={t('login.placeholder_login')} className="form-control form-input" id="pseudo" onChange={(e) => setPseudo(e.target.value)} />
+				<label htmlFor="label-upload" className="form-text">{t('complete-your-profile.upload-avatar')}</label>
+				<input type="file" name="Upload a profile photo" id="upload-profile-photo" className="upload" />
 				<button type="submit" className="btn btn-primary form-button">{t('complete-your-profile.complete-button')}</button>
 			</form>
 		</div>
