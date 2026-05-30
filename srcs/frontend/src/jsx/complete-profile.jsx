529f358e0a2ec0d713dcb2b4f7f09fe1608346a3
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-31 01:57:57 +0200

    some translation corrections

--- srcs/frontend/src/jsx/complete-profile.jsx
+++ srcs/frontend/src/jsx/complete-profile.jsx
@@ -7,14 +7,14 @@ import kindred from '../assets/avatars/kindred.png';
 import radian from '../assets/avatars/radian.png';
 import taxman from '../assets/avatars/taxman.png';
 import virtue from '../assets/avatars/virtue.png';
-
+import hershel from '../assets/avatars/hershel.webp';
 
 
 function CompleteYourProfile(){
     const {t} = useTranslation();
 	const [pseudo, setPseudo] = useState("");
 	const [avatar, setAvatar] = useState(null);
-	const defaultAvatars = [holocene, kindred, radian, taxman, virtue];
+	const defaultAvatars = [holocene, kindred, radian, taxman, virtue, hershel];
 
 	const manageSubmit = (e) => {
 		e.preventDefault();
@@ -26,7 +26,7 @@ function CompleteYourProfile(){
 			<form className="login-form d-flex flex-column gap-3" onSubmit={manageSubmit} >
 				<h1 className="login-title">{t('complete-your-profile.title')}</h1>
 				<label htmlFor="pseudo" className="form-text">{t('complete-your-profile.label-pseudo')}</label>
-				<input type="text" name="name" value={pseudo} placeholder={t('login.placeholder_login')} className="form-control form-input" id="pseudo" onChange={(e) => setPseudo(e.target.value)} />
+				<input type="text" name="name" value={pseudo} placeholder={t('complete-your-profile.placeholder_pseudo')} className="form-control form-input" id="pseudo" onChange={(e) => setPseudo(e.target.value)} />
 				<label htmlFor="upload" className="form-text">{t('complete-your-profile.upload-avatar')}</label>
 				<label htmlFor="avatar" className="btn btn-secondary btn-sm align-self-start">
     				{t('complete-your-profile.upload-avatar')}
