cdf595ea688257ee67e584724c3c53eac23475e9
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-30 21:10:34 +0200

    images and images selection

--- srcs/frontend/src/jsx/complete-profile.jsx
+++ srcs/frontend/src/jsx/complete-profile.jsx
@@ -2,11 +2,19 @@ import "../scss/login.scss" //scss file, for styling
 import "../scss/common-classes.scss"
 import { useState } from "react" //allows re-render
 import { useTranslation } from 'react-i18next';
+import holocene from '../assets/avatars/holocene.png';
+import kindred from '../assets/avatars/kindred.png';
+import radian from '../assets/avatars/radian.png';
+import taxman from '../assets/avatars/taxman.png';
+import virtue from '../assets/avatars/virtue.png';
+
+
 
 function CompleteYourProfile(){
     const {t} = useTranslation();
 	const [pseudo, setPseudo] = useState("");
 	const [avatar, setAvatar] = useState(null);
+	const defaultAvatars = [holocene, kindred, radian, taxman, virtue];
 
 	const manageSubmit = (e) => {
 		e.preventDefault();
@@ -24,9 +32,14 @@ function CompleteYourProfile(){
     				{t('complete-your-profile.upload-avatar')}
 				</label>
 				<input type="file" id="avatar" accept="image/*" style={{display: 'none'}} onChange={(e) => setAvatar(e.target.files[0])} />
-				<label htmlFor="avatar" className="form-text">
-    				{t('complete-your-profile.choose-an-avatar')}
-				</label>
+				<p className="form-text">
+					{t('complete-your-profile.choose-an-avatar')}
+				</p>
+				<div className="d-flex gap-2 justify-content-center flex-wrap avatar-grid">
+				{defaultAvatars.map((src, i) => (
+					<img key={i} src={src} alt={`avatar-${i}`} className={`img-avatar ${avatar === src ? 'selected' : ''}`} onClick={() => setAvatar(src)}/>
+				))}
+			</div>
 				<button type="submit" className="btn btn-primary btn-sm align-self-center">
     				{t('complete-your-profile.complete-button')}
 				</button>
