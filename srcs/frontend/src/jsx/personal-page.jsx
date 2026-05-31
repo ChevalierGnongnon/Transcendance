80eccdd60ee4f7ea3a1fa931fab3782ad5bde757
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-05-31 18:32:57 +0200

    Corrections

--- srcs/frontend/src/jsx/personal-page.jsx
+++ srcs/frontend/src/jsx/personal-page.jsx
@@ -1,7 +1,18 @@
+import "../scss/common-classes.scss"
+import i18n from '../../localisation/i18n.js'
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
 function PersonalPage() {
+    const {t} = useTranslation();
+
     return (
-        <div>Ma page</div>
+        
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+            <form className="login-form d-flex flex-column align-items-center gap-3">
+                <h1 className="login-title">{t('profile-page.title')}</h1>
+            </form>
+        </div>
     );
 }
-
 export default PersonalPage;
