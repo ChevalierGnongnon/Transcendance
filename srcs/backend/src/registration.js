2cb63c3d5929e2ca32fa167243080b5055dec5d1
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-01 15:37:05 +0200

    Styling login page

--- srcs/backend/src/registration.js
+++ srcs/backend/src/registration.js
@@ -1,2 +1,4 @@
+const express = require('express');
 const bcrypt = require('bcrypt');
-const database = require('db-connexion.js');
+const database = require('./db-connexion.js');
+const router = express.Router();
