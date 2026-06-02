75531a73b826cf6f4183b5a5631f7a3fe5199b4b
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-02 20:10:52 +0200

    flow on register page

--- srcs/frontend/src/jsx/main.jsx
+++ srcs/frontend/src/jsx/main.jsx
@@ -7,6 +7,7 @@ import Header from "./header";
 import Register from "./register.jsx";
 import PersonalPage from "./personal-page.jsx";
 import CompleteYourProfile from "./complete-profile.jsx";
+import RegisterFlow from "./register-flow.jsx"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -18,7 +19,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				<Route path="/register" element={<Register/>}/>
 				<Route path="/completeYourProfile" element={<CompleteYourProfile/>}/>
 				<Route path="/personalpage" element={<PersonalPage/>}/>
-				<Route path="/" element={<CompleteYourProfile/>} />
+				<Route path="/registerFlow" element={<RegisterFlow/>}/>
+				<Route path="/" element={<RegisterFlow/>} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
---
+++ srcs/frontend/src/jsx/register-flow.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Register } from "./register.jsx";
+import { CompleteYourProfile } from "./complete-profile.jsx";
+
+
+function RegisterFlow() {
+    const [step, setStep] = useState(1);
+    const [token, setToken] = useState(null);
+    return (
+        <>
+            {step === 1 && <Register onSuccess={(token) => { saveToken; setStep(2) }} />}
+            {step === 2 && <CompleteYourProfile token={token} />}
+        </>
+    )
+}
