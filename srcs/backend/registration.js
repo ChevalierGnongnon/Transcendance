e4dda1a94204c8ca9ba9d6d3da9341135d8b946f
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-02 22:47:23 +0200

    first step on backend done

--- srcs/backend/registration.js
+++ srcs/backend/registration.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const database = require('./src/db-connexion.js');
 const router = express.Router();
-
+const jwt = require('jsonwebtoken')
 
 router.post('/register', async(req, res) =>{
     const { name, last_name, email, password, passwordVerify, birthdate } = req.body;
@@ -19,18 +19,27 @@ router.post('/register', async(req, res) =>{
     if (!emailRegex.test(email))
         return (res.status(400).json({ error: 'Invalid email format' }));
     if (!passwordRegex.test(password))
-        return (res.status(400).json({ error: 'Invalid password format (1 uppercase, 1 lowercase, 1 digit and 1 special char ' }));
+        return (res.status(400).json({ error: 'Invalid password format (1 uppercase, 1 lowercase, 1 digit, 1 special char and 12 char min.)' }));
     if (password !== passwordVerify)
         return (res.status(400).json({ error: 'Passwords are not the same' }));
     if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0))
         age--;
     if (age < 18)
         return res.status(400).json({ error: 'You must be at least 18 years old' });
-    database.query('SELECT account_id FROM account WHERE email = ?', [email], (err, results) => {
-        if (err)
-            return (res.status(500).json({ error: 'Database error' }));
+    try{
+        const [results] = await database.promise().query(
+            'SELECT account_id FROM account WHERE email = ?', [email]
+        );
         if (results.length > 0)
-            return (res.status(409).json({ error: 'Email already exists!' }));
-    });    
+            return res.status(409).json({ error: 'Email already exists!' });
+        const hashed = await bcrypt.hash(password, 12);
+        const token = jwt.sign(
+            {name, last_name, email, password_hash : hashed, birthdate},
+            process.env.JWT_SECRET
+        );
+        return res.status(200).json({ token });
+    } catch (err) {
+        return (res.status(500).json({error: 'database error'}));
+    }
 })
 module.exports = router;
--- srcs/backend/server.js
+++ srcs/backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const database = require('./src/db-connexion.js');
+const registrationRouter = require('./registration.js');
 const app = express();
 const port = 3000;
 
-const registrationRouter = require('./registration.js');
-
-app.use('/api', registrationRouter);
 app.use(express.json());
+app.use('/api', registrationRouter);
 
 app.get('/health', (req, res) => {
-  res.status(200).send('OK')
+    res.status(200).send('OK')
 });
 
 app.listen(port, () => {
@@ -17,10 +16,8 @@ app.listen(port, () => {
 });
 
 database.query('SELECT 1', (err, results) => {
-  if (err) {
-      console.error('DB connection failed:', err);
-  } else {
-      console.log('DB connected!');
-  }
+    if (err)
+        console.error('DB connection failed:', err);
+    else
+        console.log('DB connected!');
 });
-
