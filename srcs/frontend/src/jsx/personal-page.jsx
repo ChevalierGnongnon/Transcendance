de33d013cf46158c1ee90730a651c4497746755e
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-08 17:31:00 +0200

    adding icons

--- srcs/frontend/src/jsx/personal-page.jsx
+++ srcs/frontend/src/jsx/personal-page.jsx
@@ -2,17 +2,28 @@ import "../scss/common-classes.scss"
 import i18n from '../../localisation/i18n.js'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
 function PersonalPage() {
-    const {t} = useTranslation();
-
+    const { t } = useTranslation();
+    const [user, setUser] = useState(null);
+if (!user) return <p>Chargement...</p>
+    useEffect(() => {
+        fetch('/api/my-profile', {
+            headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') }
+        })
+        .then(res => res.json())
+        .then(data => setUser(data))
+    }, []);
     return (
-        
-        <div className="d-flex justify-content-center align-items-center min-vh-100">
-            <form className="login-form d-flex flex-column align-items-center gap-3">
-                <h1 className="login-title">{t('profile-page.title')}</h1>
-            </form>
-        </div>
+
+        <main className="d-flex justify-content-center align-items-center min-vh-100">
+            <section className="d-flex flex-column align-items-center gap-3">
+                <figure>
+                    <img src={user.profile_photo_url} alt="avatar" className="profile-photo" />
+                </figure>
+            </section>
+        </main>
     );
 }
 export default PersonalPage;
