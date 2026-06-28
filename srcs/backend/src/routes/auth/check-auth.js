305b986920baf20121a8e86abeda321ba5fae0e6
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-28 14:26:44 +0200

    modified: check-auth route for calling check-auth-token middleware

--- srcs/backend/src/routes/auth/check-auth.js
+++ srcs/backend/src/routes/auth/check-auth.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const checkAuthToken = require('../../middlewares/check-auth-token.js');
 
-router.get('/check-auth', (req, res) => {
-    const   infos = req.cookies.token;
-    let     decoded;
-    
-    if (!infos)
-        return (res.status(401).json({error: 'USER_NOT_FOUND'}));
-    try{
-        decoded = jwt.verify(infos, process.env.JWT_SECRET);
-        if (decoded)
-            return (res.status(200).json({ authenticated: true }));
-    }  catch(err) {
-        return res.status(401).json({ error: 'INVALID_TOKEN' });
-    }
-})
+router.get('/check-auth', checkAuthToken, (req, res) => {
+    res.status(200).json({authenticated: true});
+});
 module.exports = router;
