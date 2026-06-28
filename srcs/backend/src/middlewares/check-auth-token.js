b18bb7953ecae1febe1ba714a66abf15672c58ac
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-28 15:29:56 +0200

    done: check-auth-token as middleware for authentification done

--- srcs/backend/src/middlewares/check-auth-token.js
+++ srcs/backend/src/middlewares/check-auth-token.js
@@ -1,3 +1,16 @@
 const jwt = require('jsonwebtoken');
 
-const check
+const checkAuthToken = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token)
+        return (res.status(401).json({error: 'INVALID_TOKEN'}));
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.account = decoded;
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: 'INVALID_TOKEN' });
+    }
+}
+module.exports = checkAuthToken;
--- srcs/backend/src/routes/auth/check-auth.js
+++ srcs/backend/src/routes/auth/check-auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
 const checkAuthToken = require('../../middlewares/check-auth-token.js');
 
