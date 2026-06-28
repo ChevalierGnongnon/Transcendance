f9d3d81f342c501065705b12cd9b6220ac3c2f0e
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-28 13:11:21 +0200

    files update

--- srcs/backend/src/routes/files/upload-file.js
+++ srcs/backend/src/routes/files/upload-file.js
@@ -2,9 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
 const database = require('../../config/db-connexion.js');
+const uploadConfig = require('../../config/multer-config.js');
 const fs = require('fs');
 const path = require('path');
 
 router.post('/upload', async(req, res) =>{
-
+    
 })
