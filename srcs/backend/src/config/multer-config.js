f00d7513c7ea2ce33f11f39e4649d319032c9f34
chhoflac <chhoflac@student.42.fr>
2026-06-29 14:07:35 +0200

    multer config update

--- srcs/backend/src/config/multer-config.js
+++ srcs/backend/src/config/multer-config.js
@@ -3,15 +3,8 @@ const path = require('path');
 const crypto = require('crypto');
 
 function uploadConfig(options){
-    const diskStorage = multer.diskStorage({
-        destination: (req, file, cb) => cb(null, '/app/uploads'),
-        filename : (req, file, cb) => {
-            const ext = path.extname(file.originalname);
-            cb(null, crypto.randomUUID() + ext)
-        }
-    });
     return (multer({
-        storage: diskStorage,
+        storage: multer.memoryStorage,
         limits: {fileSize: options.maxSize}, 
         fileFilter: options.fileFilter
     }));
