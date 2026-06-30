244f29b01748cf78c6046f46edf30da131064bee
chhoflac <chhoflac@student.42.fr>
2026-06-30 15:08:44 +0200

    corrections : triggers.sql

--- srcs/backend/database/scripts/triggers.sql
+++ srcs/backend/database/scripts/triggers.sql
@@ -7,14 +7,14 @@ FOR EACH ROW
 BEGIN
     IF OLD.account_type = 'admin' THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot delete admin account';
-    END IF
+    END IF;
 END //
 
 CREATE TRIGGER reset_defaut_avatar
 BEFORE DELETE ON file
 FOR EACH ROW
 BEGIN
-    DECLARE random_avatar CHAR(36)
+    DECLARE random_avatar CHAR(36);
     IF OLD.type = 'profile_photo' THEN
         SELECT file_id INTO random_avatar
         FROM file
@@ -27,6 +27,6 @@ BEGIN
         UPDATE account
         SET profile_photo_id = random_avatar
         WHERE profile_photo_id = OLD.file_id;
-    END IF
+    END IF;
 END //
 DELIMITER ;
