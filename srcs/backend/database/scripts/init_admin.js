cdcbd3424f879d59bcea5316a7bd7a750f224394
chhoflac <chhoflac@student.42.fr>
2026-07-11 16:04:37 +0200

    added: pp for admin in admin creation script

--- srcs/backend/database/scripts/init_admin.js
+++ srcs/backend/database/scripts/init_admin.js
@@ -16,6 +16,10 @@ const initAdmin = async() => {
     const gamesLost = 999;
     const personalBest = 999;
     const accountType = 'admin';
+    const ppUUID = '3ecc845f-7e97-4e9b-b01e-cce149e518c3';
+    const ppName = '3ecc845f-7e97-4e9b-b01e-cce149e518c3.webp';
+    const ppType = 'profile_photo';
+    const ppMimeType = 'image/webp';
     try{
         const [results] = await database.promise().query(
             'SELECT account_id FROM account WHERE email = ?', [emailAdmin]
@@ -24,9 +28,17 @@ const initAdmin = async() => {
             return ;
         const pwd = fs.readFileSync('/run/secrets/admin_password', 'utf8').trim();
         const hashedPWD = await bcrypt.hash(pwd, 12);
-        const [insertResults] = await database.promise().query(
+        const [insertAdminResults] = await database.promise().query(
                 'INSERT INTO account (account_id, account_type, name, last_name, email, password_hash, birthdate, pseudo, profile_photo_id, games_played, games_won, games_lost, personal_best) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                 [adminUUID, accountType, name, lastName, emailAdmin, hashedPWD, birthdate, pseudo, profile_photo_id, gamesPlayed, gamesWon, gamesLost, personalBest]
+        );
+        const [insertProfilePhotoResults] = await database.promise().query(
+            'INSERT INTO file(file_id, file_name, type, uploader_id, mime_type) VALUES(?, ?, ?, ?, ?)' , 
+            [ppUUID, ppName, ppType, adminUUID, ppMimeType] 
+        );
+        await database.promise().query(
+            'UPDATE account SET profile_photo_id = ? WHERE account_id = ?',
+            [ppUUID, adminUUID]
         );
     } catch (err){
         console.error('initAdmin failed:', err);
---
+++ srcs/backend/database/scripts/init_default_profile_pictures.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto');
+
+
+const initDefaultProdilePictures= async() => {
+    const hershel_img_id = crypto.randomUUID();
+    const holocene_img_id = crypto.randomUUID();
+    const kindred_img_id = crypto.randomUUID();
+    const radian_img_id = crypto.randomUUID();
+    const taxman_img_id = crypto.randomUUID();
+    const virtue_img_id = crypto.randomUUID();
+
+}
+
+module.exports = initDefaultProdilePictures;
+
+
