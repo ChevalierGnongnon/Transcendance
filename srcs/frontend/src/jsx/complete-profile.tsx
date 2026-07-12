56e9439df03cb4858da8ee0a5398afd2686a0ceb
chhoflac <chhoflac@student.42.fr>
2026-07-12 15:54:51 +0200

    js to ts migration

--- srcs/frontend/src/jsx/complete-profile.tsx
+++ srcs/frontend/src/jsx/complete-profile.tsx
@@ -1,16 +1,16 @@
 import "../scss/login.scss" //scss file, for styling
 import "../scss/common-classes.scss"
 import "../scss/complete-profile.scss"
-import { useState } from "react" //allows re-render
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next';
-import ErrorMessage from "./error-message.jsx";
+import { useState, FormEvent } from "react"
+import ErrorMessage from "./error-message";
 
 
 function CompleteYourProfile() {
 	const { t } = useTranslation();
 	const [pseudo, setPseudo] = useState("");
-	const [avatar, setAvatar] = useState(null);
+	const [avatar, setAvatar] = useState<File | string | null>(null);
 	const defaultAvatars = [
 		'/uploads/holocene.png',
 		'/uploads/hershel.webp',
@@ -19,10 +19,10 @@ function CompleteYourProfile() {
 		'/uploads/taxman.png',
 		'/uploads/virtue.png'
 	];
-	const [error, setError] = useState(null);
+	const [error, setError] = useState<string | null>(null);
 	const navigate = useNavigate();
 
-	const manageSubmit = async (e) => {
+	const manageSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const response = await fetch('/api/complete-profile', {
 			method: 'POST',
@@ -48,7 +48,7 @@ function CompleteYourProfile() {
 				<label htmlFor="avatar" className="btn btn-secondary btn-sm">
 					{t('complete-your-profile.upload-avatar')}
 				</label>
-				<input type="file" id="avatar" accept="image/*" style={{ display: 'none' }} onChange={(e) => setAvatar(e.target.files[0])} />
+				<input type="file" id="avatar" accept="image/*" style={{ display: 'none' }} onChange={(e) => setAvatar(e.target.files?.[0] ?? null)} />
 				<p className="form-text">
 					{t('complete-your-profile.choose-an-avatar')}
 				</p>
