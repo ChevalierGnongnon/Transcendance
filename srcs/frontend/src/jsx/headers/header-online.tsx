9cb5abde9cc8ebc6b87e41e3ae0751c1d598c1e8
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-07-29 20:37:41 +0200

    added langage switch in online header

--- srcs/frontend/src/jsx/headers/header-online.tsx
+++ srcs/frontend/src/jsx/headers/header-online.tsx
@@ -7,7 +7,13 @@ import { useState, useEffect } from "react";
 import messageIcon from '../../assets/icons/icon-messages.png';
 import parametersIcon from '../../assets/icons/icon-parameters.png';
 import notificationIcon from '../../assets/icons/icon-notifications.png'
-import logoutIcon from "../../assets/icons/icon-disconnect.png"
+import logoutIcon from "../../assets/icons/icon-disconnect.png";
+import deFlag from "../../assets/flags/de-flag.png";
+import enFlag from "../../assets/flags/en-flag.png";
+import frFlag from "../../assets/flags/fr-flag.png";
+import ruFlag from "../../assets/flags/ru-flag.png";
+import ukFlag from "../../assets/flags/uk-flag.png";
+
 
 interface User {
     name: string;
@@ -44,13 +50,20 @@ function HeaderOnline() {
         <>
             <header className="profile-page-header">
                 <section className="d-flex align-items-center gap-3">
-                    <figure className="m-0">
+                    <figure className="m-0" onClick={() => navigate('/PersonalPage')}>
                         <img src={user.file_name ? `/uploads/${user.file_name}` : '/default-avatar.png'} alt="avatar" className="img-avatar-profilePage" />
                     </figure>
                     <div className="d-flex flex-column">
                         <h3>{user.pseudo}</h3>
                         <span>{user.name} {user.last_name}</span>
                     </div>
+                </section>
+                <section className="ms-auto d-flex gap-1">
+                    <span className="lang-choice" onClick={() => { i18n.changeLanguage('fr'); localStorage.setItem('lang', 'fr'); }}>Français</span>
+                    <span className="lang-choice" onClick={() => { i18n.changeLanguage('en'); localStorage.setItem('lang', 'en'); }}>English</span>
+                    <span className="lang-choice" onClick={() => { i18n.changeLanguage('de'); localStorage.setItem('lang', 'de'); }}>Deutsch</span>
+                    <span className="lang-choice" onClick={() => { i18n.changeLanguage('ru'); localStorage.setItem('lang', 'ru'); }}>Русский</span>
+                    <span className="lang-choice" onClick={() => { i18n.changeLanguage('uk'); localStorage.setItem('lang', 'uk'); }}>українська</span>
                 </section>
                 <section className="ms-auto d-flex gap-2">
                     <figure className="m-0 pp-icons-header" data-tooltip={t('common.messages')}>
