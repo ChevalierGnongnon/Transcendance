e753807f5e8bbe393a0ea62e11dc51be06c56017
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-07-29 14:32:44 +0200

    headers fixed, deconnexion starting

--- srcs/frontend/src/jsx/config/main.tsx
+++ srcs/frontend/src/jsx/config/main.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "../register-and-login/login";
-import Header from "../headers/header-offline";
 import PersonalPage from "../account/personal-page";
 import RegisterFlow from "../register-and-login/register-flow";
 import Parameters from '../account/parameters';
-import PersonalHeader from '../headers/header-online';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import AppHeader from '../headers/app-header'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Header/>
-			<PersonalHeader/>
+			<AppHeader/>
 			<Routes>
 				<Route path="/login" element={<Login/>}/>
 				<Route path="/register" element={<RegisterFlow/>}/>
--- srcs/frontend/src/jsx/headers/app-header.tsx
+++ srcs/frontend/src/jsx/headers/app-header.tsx
@@ -1,8 +1,23 @@
 import {useState, useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 import HeaderOffline from './header-offline';
 import HeaderOnline from './header-online';
 
 function AppHeader(){
-    const isAuthentificated = useState<true | false | null>(null);
+    const [isAuthenticated, setIsAuthenticated] = useState<true | false | null>(null);
+    const location = useLocation();
+    useEffect(() => {
+        fetch('/api/check-auth', {
+            credentials: 'include',
+        })
+        .then(res => setIsAuthenticated(res.ok))
+        .catch(() => setIsAuthenticated(false));
+    }, [location.pathname]);
+    if (isAuthenticated == null)
+        return (null); // in case it takes time to check authentfication, displays nothing(no flash)
+    else if (isAuthenticated)
+        return <HeaderOnline/>
+    else
+        return <HeaderOffline/>
 }
 export default AppHeader;
