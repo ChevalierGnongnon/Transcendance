9280ff45b0f92aeb5e041f5c85f8d87e9f1cb7ad
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-08-03 14:46:31 +0200

    made a better offline header

--- srcs/frontend/src/jsx/headers/header-offline.tsx
+++ srcs/frontend/src/jsx/headers/header-offline.tsx
@@ -6,15 +6,15 @@ import { Link } from 'react-router-dom';
 function HeaderOffline(){
     const {t} = useTranslation();
     return (
-        <header className="common-head d-flex gap-2 p-2">
-            <input type="button" value="Français" className="btn btn-secondary btn-sm" onClick={() => { i18n.changeLanguage('fr'); localStorage.setItem('lang', 'fr'); }}/>
-            <input type="button" value="English" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('en'); localStorage.setItem('lang', 'en'); }}/>
-            <input type="button" value="Deutsch" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('de'); localStorage.setItem('lang', 'de'); }}/>
-            <input type="button" value="Русский" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('ru'); localStorage.setItem('lang', 'ru'); }}/>
-            <input type="button" value="українська" className="btn btn-secondary btn-sm" onClick={() => {i18n.changeLanguage('uk'); localStorage.setItem('lang', 'uk'); }}/>
+        <header className="common-head d-flex gap-2 p-3">
+            <input type="button" value="Français" className="btn btn-secondary header-btn btn-sm" onClick={() => { i18n.changeLanguage('fr'); localStorage.setItem('lang', 'fr'); }}/>
+            <input type="button" value="English" className="btn btn-secondary header-btn btn-sm" onClick={() => {i18n.changeLanguage('en'); localStorage.setItem('lang', 'en'); }}/>
+            <input type="button" value="Deutsch" className="btn btn-secondary header-btn btn-sm" onClick={() => {i18n.changeLanguage('de'); localStorage.setItem('lang', 'de'); }}/>
+            <input type="button" value="Русский" className="btn btn-secondary header-btn btn-sm" onClick={() => {i18n.changeLanguage('ru'); localStorage.setItem('lang', 'ru'); }}/>
+            <input type="button" value="українська" className="btn btn-secondary header-btn btn-sm" onClick={() => {i18n.changeLanguage('uk'); localStorage.setItem('lang', 'uk'); }}/>
             <div className="ms-auto d-flex gap-2">
-                <Link to="/login" className="btn btn-secondary btn-sm">{t('common.login')}</Link>
-                <Link to="/register" className="btn btn-secondary btn-sm">{t('common.register')}</Link>
+                <Link to="/login" className="btn btn-secondary btn-sm header-btn">{t('common.login')}</Link>
+                <Link to="/register" className="btn btn-secondary btn-sm header-btn">{t('common.register')}</Link>
             </div>
         </header>
     );
