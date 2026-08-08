c55988082e26e4fe3b658368e129218faf639958
chhoflac <chhoflac@student.42.fr>
2026-08-08 17:18:55 +0200

    personal page on web version done

--- srcs/frontend/src/jsx/account/personal-page.tsx
+++ srcs/frontend/src/jsx/account/personal-page.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import messageShortcut from "../../assets/icons/message-shortcut.png"
 import updateShortcut from "../../assets/icons/update-shortcut.png"
-
+import playShortcut from "../../assets/icons/play-shortcut.png"
 
 interface User {
     name: string;
@@ -28,32 +28,39 @@ function PersonalPage() {
     }, []);
 
     if (!user)
-        return <p>Chargement...</p>;
+        return <p>{t('common.loading')}</p>;
     return (
         <>
             <main className="d-flex flex-column justify-content-center align-items-center">
-                <img src={user.file_name ? `/uploads/${user.file_name}` : '/default-avatar.png'} alt="avatar" className="img-avatar-profile-page" />
-                <div className="d-flex gap-2 justify-content-center flex-wrap shortcut-grid">
-                    <figure className="shortcut-icon">
-                        <img src={messageShortcut} alt="message-shortcut" />
-                        <span>Messages</span>
-                    </figure>
-                    <figure className="shortcut-icon">
-                        <img src={updateShortcut} alt="message-shortcut" />
-                        <span>Update my profile</span>
-                    </figure>
-                </div>
-                <span>Game stats</span>
-                <div className="d-flex gap-2 justify-content-center flex-wrap shortcut-grid">
-                    <figure className="shortcut-icon">
-                        <span>Games won</span>
-                    </figure>
-                    <figure className="shortcut-icon">
-                        <span>Games lost</span>
-                    </figure>
-                    <figure className="shortcut-icon">
-                        <span>Best score</span>
-                    </figure>
+                <div className="profile-page d-flex flex-column gap-3 justify-content-center align-items-center min-vh-100">
+                    <img src={user.file_name ? `/uploads/${user.file_name}` : '/default-avatar.png'} alt="avatar" className="img-avatar-profile-page" />
+                    <span>{t('profile-page.go-to')}</span>
+                    <div className="d-flex gap-2 justify-content-center flex-wrap shortcut-grid">
+                        <figure className="shortcut-icon">
+                            <img src={messageShortcut} alt="message-shortcut" />
+                            <span>{t('common.messages')}</span>
+                        </figure>
+                        <figure className="shortcut-icon">
+                            <img src={updateShortcut} alt="message-shortcut" />
+                            <span>{t('common.update-my-profile')}</span>
+                        </figure>
+                        <figure className="shortcut-icon">
+                            <img src={playShortcut} alt="play-shortcut" />
+                            <span>{t('common.play')}</span>
+                        </figure>
+                    </div>
+                    <span>{t('profile-page.game-stats')}</span>
+                    <div className="d-flex gap-2 justify-content-center flex-wrap shortcut-grid">
+                        <figure className="shortcut-icon">
+                            <span>{t('profile-page.games-won')}</span>
+                        </figure>
+                        <figure className="shortcut-icon">
+                            <span>{t('profile-page.games-lost')}</span>
+                        </figure>
+                        <figure className="shortcut-icon">
+                            <span>{t('profile-page.best-score')}</span>
+                        </figure>
+                    </div>
                 </div>
             </main>
         </>
