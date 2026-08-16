19dfeca59b6f2d359ff8647baa4d47580907bd6c
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-08-16 11:41:07 +0200

    code commentary

--- srcs/frontend/src/jsx/auth/auth-context.tsx
+++ srcs/frontend/src/jsx/auth/auth-context.tsx
@@ -1,5 +1,8 @@
 import { createContext, useContext, useState, useEffect, useCallback, ReactNode } from "react";
 
+// Shape of the info shared everywhere in the app (via AuthProvider):
+// whether or not the user is connected,
+// and functions to change its status and check if it changed
 type AuthContextType = {
     isAuthenticated: true | false | null;
     login: () => void;
@@ -9,17 +12,24 @@ type AuthContextType = {
 
 const context = createContext<AuthContextType | undefined>(undefined);
 
+// Auth provider : delivers connection status (is authenticated)
+// and the functions to change the status (login / logout / refresh)
+// info everywhere inside its tags
 export function AuthProvider({children}:{children:ReactNode}){
     const [isAuthenticated, setIsAuthenticated] = useState<true | false | null>(null)
     
+    //simple function to avoid code repetition, checks/fetches the current connection status
     const checkAuth = async() => {
+        //calls check-auth route to know if the token cookie is still valid
         await fetch('/api/check-auth',{
                 credentials: 'include'
         })
         .then(res => setIsAuthenticated(res.ok))
         .catch(() => setIsAuthenticated(false));
-
     }
+
+    // useEffect: used to trigger checkauth() one time, when the app is starting.
+    // [] at the end is for "trigger it once, never restarted after"
     useEffect(() => {
         checkAuth();        
     }, []);
@@ -42,12 +52,17 @@ export function AuthProvider({children}:{children:ReactNode}){
 
     
     return (
+        //returns value + usefull functions
         <context.Provider value={{ isAuthenticated, login, logout, refresh }}>
             {children}
         </context.Provider>
     )
 }
 
+// useAuth: entry point every other component uses to read and act on the auth state
+// (isAuthenticated, login, logout, refresh) without touching useContext directly,
+// it throws if called outside AuthProvider, so if it's missing, the  provider fails loudly
+// instead of silently breaking somewhere else.
 export function useAuth(){
     const ctx = useContext(context);
 
