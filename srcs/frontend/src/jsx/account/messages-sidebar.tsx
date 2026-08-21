abb7c3d1ff5fe6387d48780a8793985b0906afdc
chhoflac <chhoflac@student.42.fr>
2026-08-21 11:39:10 +0200

    sidebar done

--- srcs/frontend/src/jsx/account/messages-sidebar.tsx
+++ srcs/frontend/src/jsx/account/messages-sidebar.tsx
@@ -2,18 +2,21 @@ import messageIcon from "../../assets/icons/message-60.png"
 import moreIcon from "../../assets/icons/plus.png"
 import blockIcon from "../../assets/icons/croix.png"
 import geminiIcon from "../../assets/icons/gemini.png"
+import { useTranslation } from "react-i18next"
 
 function SideBar(){
+    const {t} = useTranslation();
+
     return (
         <div className="d-flex flex-column align-items-center justify-content-start gap-4 sidebar-nav py-3 m-2 side-bar px-2">
             <div className="icon-message-message m-2"><img src={messageIcon} alt="messagelist" /></div>
-            <span>Mes messages</span>
+            <span>{t('message.my-messages')}</span>
             <div className="icon-message-message m-2"><img src={moreIcon} alt="newone" /></div>
-            <span>Nouveau message</span>
+            <span>{t('message.new-message')}</span>
             <div className="icon-message-message m-2"><img src={blockIcon} alt="block" /></div>
-            <span>Bloquer</span>
+            <span>{t('message.block')}</span>
             <div className="icon-message-message m-2"><img src={geminiIcon} alt="gemini" /></div>
-            <span>Gemini</span>
+            <span>{t('message.imaginary-friend')}</span>
         </div>
     );
 }
