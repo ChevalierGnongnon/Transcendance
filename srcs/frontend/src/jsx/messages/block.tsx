80533f30a1c4de79169e9edae20514081847bfd0
chhoflac <chhoflac@student.42.fr>
2026-08-22 19:28:24 +0200

    message done on frontend part

--- srcs/frontend/src/jsx/messages/block.tsx
+++ srcs/frontend/src/jsx/messages/block.tsx
@@ -8,9 +8,10 @@ function Block(){
     return (
         <form className="form-new-chat py-3 my-2 gap-3 d-flex flex-column justify-content-center align-items-center">
             <h1>{t('message.block')}</h1>
+            <h3>{t('message.search-user')} :</h3>
             <input type="search" name="search-recipient" id="search-recipient" className="search-input" placeholder={t('common.search-user')}/>
             <input type="button" value={t('common.search')} className="new-msg-button btn btn-primary" />
-            <h1>{t('message.give-reason')}</h1>
+            <h3>{t('message.give-reason')} :</h3>
             <textarea name="message" id="message" className="new-message"></textarea>
             <input type="button" value={t('message.block')} className="new-msg-button btn btn-primary"/>
         </form>
