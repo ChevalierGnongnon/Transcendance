4db5759b273619aefe206a54c0a2d8fcc44ba3ac
chhoflac <chhoflac@student.42.fr>
2026-08-27 14:21:51 +0200

    added: pp treatment

--- srcs/backend/src/modules/files/files.services.ts
+++ srcs/backend/src/modules/files/files.services.ts
@@ -1,5 +1,11 @@
 import { prisma } from '../../lib/prisma.ts';
 import { NotFoundError } from '../../common/errors.js';
+import { UnsupportedFileTypeError } from '../../common/errors.js';
+import { avatarWhiteList, messageFileWhiteList } from './files.middlewares.ts';
+import { fileTypeFromBuffer } from 'file-type';
+import sharp from 'sharp';
+import { randomUUID } from 'crypto';
+import fs from 'fs';
 
 class FileService {
   async getDefaultAvatars() {
@@ -18,8 +24,25 @@ class FileService {
     }
     return avatars;
   }
-  async createFile() {
-      
+  async createFile(fileBuffer: Buffer,  userId: string, type: string) {
+    const fileType = await fileTypeFromBuffer(fileBuffer);
+    
+    if (fileType === undefined)
+      throw new UnsupportedFileTypeError('Unsupported file type by fileTypeFromBuffer()');
+    else {
+      if (type === 'profile_photo' && avatarWhiteList.includes(fileType.mime)){
+        const result = await sharp(fileBuffer).toFormat('webp').toBuffer();
+        const id = randomUUID();
+        const fileName = `${id}.webp`;
+        fs.writeFileSync(`/app/uploads/${fileName}`, result)
+      }
+      else if (type === 'message' && messageFileWhiteList.includes(fileType.mime)){
+        
+      }
+      else {
+        throw new UnsupportedFileTypeError('File type is not in whitelist');
+      }
+    }
   }
 }
 
