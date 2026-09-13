1e1437d124cc6fb7cfa869f99ed0769852439f67
chhoflac <chhoflac@student.42.fr>
2026-09-13 16:09:46 +0200

    added : profile page for other users

--- srcs/frontend/src/jsx/config/main.tsx
+++ srcs/frontend/src/jsx/config/main.tsx
@@ -32,7 +32,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 					<Route path="/addfriend" element={<RequireAuth><AddFriend /></RequireAuth>} />
 					<Route path="/mystats" element={<RequireAuth><MyStats /></RequireAuth>} />
 					<Route path="/myfriends" element={<RequireAuth><MyFriends /></RequireAuth>} />
+					<Route path="/profile/:pseudo" element={<RequireAuth><PersonalPage /></RequireAuth>} />
 					<Route path="/" element={<GuestOnly><Login/></GuestOnly>} />
+
 				</Routes>
 				<Footer />
 			</AuthProvider>
